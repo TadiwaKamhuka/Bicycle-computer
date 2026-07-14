a90f8f88406cef647faa60ee61b4cbb98c94a259
Tadiwa Kamhuka <110743589+TadiwaKamhuka@users.noreply.github.com>
2026-07-14 17:35:59 +0200

    Add file containing functions
    Functions are related to gps data reading/writing and parsing messages from each of the modules

--- gps_info.py
+++ gps_info.py
@@ -1,67 +1,66 @@
-from machine import UART, Pin
-import time
-
-# Setup uart for communication and to gather information from gps module
-# id=2 for gpio 16/17
-uart = UART(2, baudrate=115200, rx=16, tx=17)
-gps_data = {"module": []}
-
-# Go through the message recieved from gps and put it in a usable format
-def parseSerialMessage(rawMessage):
-    # Use the pdop value from first GSA sentence
-    pdopCount = 0
-    
-    # Clean the string and split it into a list
-    if rawMessage[0:2] == "b'":
-        rawMessage = rawMessage.lstrip("b'").rstrip("'")
-    messages = rawMessage.split("$")
+# Clean the string and split it into a list
+def cleanMessage(rawMessage):
+    return rawMessage.split("$")
         
+# Parse list for valueable info
+def parseList(cleanMessage, filename): 
     result = {}
-    
-    for message in messages:
-        # Message id acquired to select only necessary messages
+    for message in cleanMessage:
+    # Message id acquired to select only necessary message type
         message_id = message[2:5]
         
         # GGA message contains most positional data
         if message_id == "GGA":
             gga = message.split(',')
-            result["time"] = gga[1]
-            result["latitude"] = gga[2] + gga[3]
-            result["longitude"] = gga[4] + gga[5]
-            result["satellites"] = int(gga[7])
-            result["altitude"] = gga[9] + gga[10]
-        
-        # GSA contains satellite data and data for accuracy checking
-        if message_id == "GSA":
-            gsa = [g for g in message.split(',') if g != '']
-            
-            # Only record first GSA sentences pdop value
-            if pdopCount == 0:
-                result["pdop"] = gsa[-4]
-                pdopCount += 1
-            
+            if len(gga) >= 9:
+                result["time"] = gga[1]
+                result["latitude"] = gga[2]
+                result["latitude_dir"] = gga[3]
+                result["longitude"] = gga[4]
+                result["longitude_dir"] = gga[5]
+                # result["fix"] = gga[6] # Delete after test
+                result["satellites"] = gga[7]
+                result["altitude"] = gga[9]            
+
+    lat = result.get("latitude")
+    lon = result.get("longitude")
+
+    # Store the coordinates into a file so they can be read later
+    storeResults(filename, [lat, lon])
+    
     return result
 
-######## Validation/Testing ############
-# Counter used to end loop after three iterations
-iterations = 1
+# Used to write the coordinates into the file so they can be read later
+def storeResults(filename, resultList): 
+    # Latitude
+    ns = resultList[0]
+    # Longitude
+    ew = resultList[1] 
+    with open(filename, "a") as f:
+        f.write(f"{nmeaToDecimal(ns)}, {nmeaToDecimal(ew)}\n")
+    
+# Convert NMEA coordinates to decimal degrees
+def nmeaToDecimal(coord):
+    # Check if the coordinate is invalid
+    if not coord:
+        return None
+    # Remove the direction letters from the coordinate
+    coord = coord.replace("N", "").replace("S", "").replace("E", "").replace("W", "")
+    
+    coord = float(coord)
+    degrees = int(coord//100)
+    minutes = coord - (degrees*100)
 
-# Allow 30 seconds to pass incase of cold start
-time.sleep(30)
+    return degrees + minutes/60
 
-# Read in information from gps module
-while True:
-    if uart.any():
-        # Message is encoded as bytes so decoding necessary to convert to string
-        rawMessage = uart.read().decode()
-        measurement = parseSerialMessage(rawMessage)
-        print(measurement)
-        gps_data["LC86G"].append(measurement)
+# filename parameter must receive an already opened file
+def fetchResults(filename):
+    path = []
 
-    if iterations == 4:
-        break
-    else:
-        iterations += 1
-        # 300s / 5m used to measure precision at different times
-        time.sleep(300)
-        
+    with open(filename, "r") as f:
+        for line in f:
+            coord = line.strip()
+            lat, lon = coord.split(",")
+            path.append(f"{lat.strip()},{lon.strip()}")
+
+    return path
